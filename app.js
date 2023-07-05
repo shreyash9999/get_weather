f1a20d8f3dfba04bdb9e2925469317bccf9e9c82
shreyash9999 <dongareshreyash20@gmail.com>
2023-07-05 19:20:54 +0530

    almost final

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const https = require("https")//basically used to request data//dont need to install the nodule with npm as its inbuit/preavaiable
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=18.516726&lon=73.856255&appid=7f47e552d6f822c850cb929bbc17edc0"
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+    const city = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7f47e552d6f822c850cb929bbc17edc0"
     
     https.get(url,function(resp){
         console.log(resp.statusCode);
@@ -15,7 +23,7 @@ app.get("/", function(req,res){
             const weatherDisp = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon
             const iconUrl=  "https://openweathermap.org/img/wn/"+icon+"@2x.png"
-            res.write("<h1>Temp in pune is : "+ temp+ "</h1><h3>discription : " + weatherDisp+"</h3>");
+            res.write("<h1>Temp in "+city+" is : "+ temp+ "</h1><h3>discription : " + weatherDisp+"</h3>");
             res.write("<img src="+ iconUrl+ ">");
             res.send();
 
@@ -23,8 +31,11 @@ app.get("/", function(req,res){
         })
     });
 
-
 });
+
+
+
+
 
 
 
